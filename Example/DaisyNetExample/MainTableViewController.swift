9c1385b2e7be4c8f1c3601cdbc8c8fc6c1d84ed5
mengqingzheng <mengqingzheng@munimob.com>
2021-12-26 14:10:24 +0800

    结构调整/删除下载

---
+++ Example/DaisyNetExample/MainTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewController.swift
+//  DaisyNet
+//
+//  Created by mengqingzheng on 2017/10/11.
+//  Copyright © 2017年 MQZHot. All rights reserved.
+//
+
+import Alamofire
+import DaisyNet
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(NSHomeDirectory())
+    }
+
+    @IBAction func clearCache(_ sender: UIBarButtonItem) {
+        DaisyNet.removeAllCache { result in
+            print(result ? "清理成功" : "清理失败")
+        }
+    }
+}
